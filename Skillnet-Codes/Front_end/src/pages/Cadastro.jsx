cab6dc3d31d8cdb6fe0a340e223bc5fd0534e1a0
CAIO LORRAM VALENTE <caio_valente@estudante.sesisenai.org.br>
2025-03-05 17:20:02 -0300

    tela de Cadastro pronta

--- Skillnet-Codes/Front_end/src/pages/Cadastro.jsx
+++ Skillnet-Codes/Front_end/src/pages/Cadastro.jsx
@@ -1,25 +1,30 @@
-import React from 'react'
+import React, { use, useState } from 'react'
 import './Cadastro.css'
 function Cadastro() {
+  const [checkBox,setCheckBox]=useState()
+
+  
   return (
     <div >
       <div className='container'>
 
-      <img className='imgCadastro' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8F1-mZskCCUZidmPBIM60PPKu0zhiXqXA6A&s" alt="" />
+      <img className='imgCadastro' src="./images/Cara_cadastro.svg" alt="" /> 
 
       <div className='container_cadastro'>
-        <div className='containerLogin'><button>login</button></div>
+        <div className='containerLogin'><button className='btnLogin'>LOGIN</button></div>
         <div><h1 className='cadatroH1'><span className='spanH1'>Crie</span> sua conta ! </h1></div>
         <div className='divInputs'>
 
-          <div className='container_inputs'><label htmlFor="">Nome de isuario</label><input type="text" className='inpt'/></div>
+          <div className='container_inputs'><label htmlFor="" className='lblCadastro'>Nome de Usuario</label><input type="text" className='inpt'/></div>
 
-          <div className='container_inputs'><label htmlFor="">Email</label> <input type="text" className='inpt'/></div>
+          <div className='container_inputs'><label htmlFor="" className='lblCadastro'>Email</label> <input type="text" className='inpt'/></div>
 
-          <div className='container_inputs'><label htmlFor="">Senha</label> <input type="password" className='inpt' /></div>
+          <div className='container_inputs'><label htmlFor="" className='lblCadastro'>Senha</label> <input type="password" className='inpt' /></div>
+
+          <div className='divCheckBox'><div className='inptCheck'></div><p className='pCheck'><span className='spanCheck'>Aceito os</span> termos de serviços</p></div>
 
         </div>
-        <button type='submit' className='btnCadastro'>Cadastrar</button>
+        <button type='submit' className='btnCadastro'>CADASTRAR</button>
        </div>
       </div>
 
--- Skillnet-Codes/Front_end/src/pages/Login.jsx
+++ Skillnet-Codes/Front_end/src/pages/Login.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Login.css'
 function Login() {
+
+
   return (
     <div>
 <div className='container'>
