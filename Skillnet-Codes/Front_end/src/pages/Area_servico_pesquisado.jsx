f7f16efcf8b3cc6064d4df33616a5ae10bf53893
VITHOR LORRAM VALENTE <vithor_valente@estudante.sesisenai.org.br>
2025-03-19 15:42:46 -0300

    coloquei mais info no readme

--- Skillnet-Codes/Front_end/src/pages/Area_servico_pesquisado.jsx
+++ Skillnet-Codes/Front_end/src/pages/Area_servico_pesquisado.jsx
@@ -4,7 +4,31 @@ import "./Area_servico_pesquisado.css"
 
 function Area_servico_pesquisado() {
  const[valorInptPesquisa,setValorInptPesquisa] = useState("")
+    let cardTrabalhosRango = 
+    {titulo:'rango',
+    snipose:'Um camaleão que viveu como um animal de estimação se encontra em uma crise de identidade. Rango se questiona',
+    capa:'https://i.scdn.co/image/ab67616d0000b27306ed1d9098d927cd5eabd758'
     
+    
+    
+    }
+    let cardTrabalhosDuna = 
+    {titulo:'Duna',
+    snipose:'Um camaleão que viveu como um animal de estimação se encontra em uma crise de identidade. Rango se questiona',
+    capa:'https://upload.wikimedia.org/wikipedia/pt/a/a3/Dune_2021.jpeg'
+    
+    
+    
+    }
+    let cardTrabalhosClick = 
+    {titulo:'Click',
+    snipose:'Um camaleão que viveu como um animal de estimação se encontra em uma crise de identidade. Rango se questiona',
+    capa:'https://m.media-amazon.com/images/S/pv-target-images/e4232de175bdb04555da6115c40d948ed477d80cba98ad39185b2534034328f3.jpg'
+    
+    
+    
+    }
+
     return (
     <div>
    
@@ -31,8 +55,30 @@ type="text"
 </div>
      
       </div>
-   <h1 className='h1_serviços_dois'>{valorInptPesquisa}</h1>
-     <div className='container_servicos'></div>
+   <h1 className='h1_serviços_dois'>{valorInptPesquisa || 'serviço'}</h1>
+     <div className='container_servicos'>
+<div className='card_serviço'>
+<h1>{ cardTrabalhosClick.titulo}</h1>
+<img src={ cardTrabalhosClick.capa} alt=""  className='img_card' />
+<p className='sinopse_card'>{ cardTrabalhosClick.snipose}</p>
+
+</div>
+<div className='card_serviço'>
+<h1>{cardTrabalhosDuna.titulo}</h1>
+<img src={cardTrabalhosDuna.capa} alt=""  className='img_card' />
+<p className='sinopse_card'>{cardTrabalhosDuna.snipose}</p>
+
+</div>
+<div className='card_serviço'>
+<h1>{cardTrabalhosRango.titulo}</h1>
+<img src={cardTrabalhosRango.capa} alt=""  className='img_card' />
+<p className='sinopse_card'>{cardTrabalhosRango.snipose}</p>
+
+</div>
+
+
+
+     </div>
        
        
         </div>
