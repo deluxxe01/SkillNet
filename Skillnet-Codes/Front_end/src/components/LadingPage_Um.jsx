f3f194b6062616fcc7316f81da5ab6a4fc12131d
caiolorramvalente <caio_valente@estudante.sesisenai.org.br>
2025-04-02 17:04:57 -0300

    Check box feita com um a div pronta e salvamento do usuario no back pronto

--- Skillnet-Codes/Front_end/src/components/LadingPage_Um.jsx
+++ Skillnet-Codes/Front_end/src/components/LadingPage_Um.jsx
@@ -2,24 +2,28 @@ import React from 'react'
 import './LadingPage_Um.css'
 import './Header.jsx'
 import Header from './Header.jsx'
+import { useNavigate } from 'react-router-dom'
 
 function LadingPage_Um() {
+  const navigate = useNavigate()
   return (
     <div>
          <Header />
         <div className='div'>
          <div>
             <h1 className='h1TitleLanding'>
-              {/* <span className='spanTitleLanding'> Conecte-se com os melhores</span><br />freelancers e alcance seus objetivos. */}
-              <span className='spanTitleLanding'> Olha o README de vocês da SA to tentando falar com vocês a dias</span><br />
+               <span className='spanTitleLanding'> Conecte-se com os melhores</span><br />freelancers e alcance seus objetivos. 
+          
             </h1>
          </div>
          <div className='containerH3'>
           <h3 className='h3Landing'>Encontre o profissional ideal para o seu projeto ou <br /> exiba suas habilidades e conquiste novos clientes.</h3>
          </div>
          <div>
-          <button className='btnLanding1'>Iniciar Sessão</button>
-          <button className='btnLanding2'>Começar</button>
+          <button className='btnLanding1' onClick={()=>{navigate("/Cadastro")}}>Iniciar Sessão</button>
+          <button className='btnLanding2' onClick={()=>{
+            navigate('/login')
+          }}>Começar</button>
          </div>
         </div>
     </div>
