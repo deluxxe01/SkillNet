ac5f48d56c1923b9588e1f06c13247de8afaa6a1
BrunoZarowne <brunowillianzarowne@gmail.com>
2025-04-23 22:54:08 -0300

    Add files via upload

---
+++ TelasSa/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    {/* <GlobalContextProvider>
+      <App />
+    </GlobalContextProvider>   */}
+    </>
+  )
+}
+
+export default App
---
+++ TelasSa/src/main.jsx
@@ -0,0 +1,16 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+import { RouterProvider } from 'react-router-dom'
+import router from './router/routes.jsx'
+import { GlobalContextProvider } from './contexts/GlobalContext.jsx'
+import CarouselPortifolio from './components/CarouselPortifolio.jsx'
+// import { ReactDOM } from 'react'
+
+createRoot(document.getElementById('root')).render(
+  <GlobalContextProvider>
+    <RouterProvider router={router}>
+    </RouterProvider>
+  </GlobalContextProvider>
+)
+
