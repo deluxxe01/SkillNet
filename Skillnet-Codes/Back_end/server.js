069a7f39489a51038d4c4dbef8535ccb793f0661
VITHOR LORRAM VALENTE <vithor_valente@estudante.sesisenai.org.br>
2025-06-02 16:07:44 -0300

    crud de servicos pronto

--- Skillnet-Codes/Back_end/server.js
+++ Skillnet-Codes/Back_end/server.js
@@ -84,6 +84,51 @@ App.post('/login_user',async(req,res)=>{
 
 
 })
+// Rota para obter serviços
+App.get('/servicos/:id', async (req, res) => {
+    const Servico = await db.selectServico(req.params.id)
+    res.json(Servico)
+  })
+  
+  
+  
+  // Rota para obter serviços
+  App.get('/servicos', async (req, res) => {
+    const Servicos = await db.selectServicos()
+    res.json(Servicos)
+  })
+  
+  
+  // Rota para obter serviços
+  App.post('/servicos', async (req, res) => {
+   await db.insertServico(req.body); 
+    res.sendStatus(201)
+  
+  
+  })
+  
+  
+  // Rota para obter serviços
+  App.patch('/servicos/:id', async (req, res) => {
+   await db.updateServico(req.params.id,req.body); 
+    res.sendStatus(200)
+  
+  
+  })
+  
+  // Rota para obter serviços
+  App.delete('/servicos/:id', async (req, res) => {
+   await db.deleteServico(req.params.id); 
+    res.sendStatus(204)
+  
+  
+  })
+  
+
+
+
+
+
 
 
 io.on("connection", socket =>{
--- Skillnet-Codes/Back_end/sql/tableUsuarios.sql
+++ Skillnet-Codes/Back_end/sql/tableUsuarios.sql
@@ -2,11 +2,11 @@ CREATE TABLE IF NOT EXISTS usuarios (
     id_usuario serial PRIMARY KEY,
     nome varchar(100),
     email varchar(200) UNIQUE,
-    senha varchar(60)  -- Considere aumentar o tamanho para armazenar senhas com hash
+    senha varchar(60)
 );
 
 CREATE TABLE IF NOT EXISTS servicos (
-    id_servico serial PRIMARY KEY,
+    servico_id serial PRIMARY KEY,
     titulo varchar(80),
     descricao varchar(500),
     capa bytea
@@ -15,7 +15,7 @@ CREATE TABLE IF NOT EXISTS servicos (
 CREATE TABLE IF NOT EXISTS portifolios (
     id_portifolio serial PRIMARY KEY,
     nome varchar(60),
-    experiencia varchar(400),  -- Corrigido o erro de digitação
+    experiencia varchar(400),
     localTrabalho varchar(300),
     especialidade text[],
     img1 bytea,
@@ -27,18 +27,18 @@ CREATE TABLE IF NOT EXISTS portifolios (
 
 CREATE TABLE IF NOT EXISTS comentarioPortifolio (
     id_comentario SERIAL PRIMARY KEY,
-    comentario text,  -- Usando 'text' para suportar comentários mais longos
+    comentario text,
     fk_Usuario_id INT,
-    fk_portifolio_id INT,  -- Corrigido o nome da coluna
+    fk_portifolio_id INT,
     CONSTRAINT fk_usuario_comentario_portifolio FOREIGN KEY (fk_Usuario_id) REFERENCES usuarios(id_usuario),
     CONSTRAINT fk_portifolio_comentario FOREIGN KEY (fk_portifolio_id) REFERENCES portifolios(id_portifolio)
 );
 
 CREATE TABLE IF NOT EXISTS comentarioServico (
     id_comentario SERIAL PRIMARY KEY,
-    comentario text,  -- Usando 'text' para suportar comentários mais longos
+    comentario text,
     fk_servico_id INT,
     fk_Usuario_id INT,
-    CONSTRAINT fk_servico FOREIGN KEY (fk_servico_id) REFERENCES servicos(id_servico),
-    CONSTRAINT fk_usuario_comentario_servico FOREIGN KEY (fk_Usuario_id) REFERENCES usuarios(id_usuario)  -- Renomeado para evitar conflito
+    CONSTRAINT fk_servico FOREIGN KEY (fk_servico_id) REFERENCES servicos(servico_id),
+    CONSTRAINT fk_usuario_comentario_servico FOREIGN KEY (fk_Usuario_id) REFERENCES usuarios(id_usuario)
 );
