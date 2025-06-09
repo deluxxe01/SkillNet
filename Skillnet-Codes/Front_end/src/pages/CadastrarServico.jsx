3d70230708293707ccedc749fe7f645f84e88b18
VITHOR LORRAM VALENTE <vithor_valente@estudante.sesisenai.org.br>
2025-06-09 17:11:40 -0300

    tela de cadastrar serviço pronta(front end)

--- Skillnet-Codes/Front_end/src/pages/CadastrarServico.jsx
+++ Skillnet-Codes/Front_end/src/pages/CadastrarServico.jsx
@@ -6,45 +6,80 @@ function CadastrarServico() {
   return (
     <div className>
         <div><Header /></div>
-        <div><h1>Cadastro de seviços</h1></div>
+        <div className='container_h1_titulo_servico'><h1 className='h1_titulo_servico'>Cadastro de seviços</h1><div className='div_linha_verde_cadastra_servico'></div></div>
+        
+        
         <div className='containerServicos'>
+         
+         <div className='container_all_inputs' >
+
          <div className='containerTituloServico'> 
-            <div className='divInputsServico'>
-                <label htmlFor="">Titulo</label>
-                <input type="text" className='inptServicos' />
+            
+            <div className='div_Inputs_servico_titulo'>
+                <label htmlFor="" >Titulo</label>
+                <input type="text" className='inpt_titulo_servico'  placeholder='Titulo do seu serviço'/>
             </div>
 
-            <div className='divInputsServico'>
+            <div className='div_Inputs_servico_descricao'>
                 <label htmlFor="">descrição</label>
-                <input type="text"  className='inptServicos' />
+               <textarea name="" id="" className='text_descrição_servico' placeholder='escreva detalhadamente sobre seu serviço'></textarea>
             </div>
 
         </div>
         <div className='containerInfoServicos'> 
             <div className='divInputsServico'>
                 <label htmlFor="">tempo de entrega</label>
-                <input type="text"  className='inptServicos' />
+                <input type="text"  className='inptServicos'  placeholder='10-11 dias'/>
             </div>
 
             <div className='divInputsServico'>
                 <label>img URL</label>
-                <input type="text" />
+                <input type="text"  className='inptServicos' placeholder='https://exemplo.com/sua-imagem.jpg'/>
             </div>
             <div className='divInputsServico'>
                 <label htmlFor="">Escolha a área que define seu serviço</label>
-                <input type="text"  className='inptServicos' />
+                <select name="" id="" className='select_area_servico'>
+                <option value='faixa' disabled selected>
+              escolha umas das opções
+            </option>
+                <option value="">matador de rato</option>
+                <option value="">desenvolvimento de software</option>
+                <option value="">back end devoloper</option>
+                <option value="">scrum master</option>
+                
+                </select>
             </div>
         </div>
          <div className='containerInfoServico2'>
+           <div className='container_preco_idiomas'>
+
             <div  className='divInputsServico'>
                 <label htmlFor="">faixa de preço</label>
-                <input type="text"  className='inptServicos' />
+                <input type="text"  className='inptServicos'placeholder='R$200 - R$1000' />
             </div>
             <div className='divInputsServico'>
-                <label htmlFor="">idiomas</label>
-                <input type="text"  className='inptServicos' />
+                <label htmlFor="">idiomas que o serviço atende</label>
+                <input type="text"  className='inptServicos' placeholder='espanhol,portugues,ingles'/>
+         
             </div>
+
+           </div>
+
          </div>
+         </div>
+         <div className='container_buttons'>
+
+<button className='botton_edita_servico'>editar serviço</button>
+
+<button className='button_cria_servico'>criar serviço</button>
+
+<button className='butonn_cancela_servico'>cancelar</button>
+
+
+         </div>
+
+
+
         </div>
     </div>
   )
