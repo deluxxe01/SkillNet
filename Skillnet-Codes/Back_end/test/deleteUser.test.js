a0a7fa460a1a2ae6463cd85fbefccbcb04dd5f16
caiolorramvalente <caio_valente@estudante.sesisenai.org.br>
2025-06-24 17:10:14 -0300

    tela sobre nós feita

--- Skillnet-Codes/Back_end/test/deleteUser.test.js
+++ Skillnet-Codes/Back_end/test/deleteUser.test.js
@@ -3,7 +3,7 @@ const{deleteUser} = require('../db/db')
 
 describe('teste unitario para vaerrificar seu a função esta apangando usuarios',()=>{
 
-  let id = 22
+  const id = 22
 
  test('deve apagar um usuário existente com sucesso', async () => { // Este bloco 'test' PODE ser async
          await expect(deleteUser(id)).toBe(true); // Esta asserção é válida dentro de um test/it async
--- Skillnet-Codes/Front_end/src/pages/PageSobreNos.jsx
+++ Skillnet-Codes/Front_end/src/pages/PageSobreNos.jsx
@@ -15,20 +15,20 @@ function PageSobreNos() {
     </div>
    </div>
     <div className='containerSobreNos'>
-      <div>
+      <div className='containerImgSobrenos'>
         <img className='imgSobrenos' src="./images/bemsa.png" alt="" />
       </div>
       <div className='containerPaiAbsolute'>
         <div className='containerPaiSobreNos'>
           <div>
            <h1 className='h1Objetivo'>Objetivos e Propósitos</h1>
-           <div className='divImg'>oi</div>
+           <div className='divImg'>
+            <img src="./images/telaFigmaHome.png" alt="" />
+           </div>
           </div>
           <div className='containerPsobreNos'>
             <div className='containerPsobreNos2'>
-              <section className='sectionSobreNos'>
-                <div>
-                    <div>
+                   <div>
                       <span className='spanP'> Oferecer soluções criativas e acessíveis</span> 
                       <p className='pSobrenos'>
                         Proporcionar soluções inovadoras e acessíveis para empresas e empreendedores.
@@ -37,21 +37,71 @@ function PageSobreNos() {
                     <div>
                       <span className='spanP'> Garantir entregas de qualidade</span> 
                       <p className='pSobrenos'>
-                    
                       Assegurar a qualidade nas entregas de diversos tipos de projetos
                       </p>
                   </div>
                 </div>
                  <img className='imgColunaSobreNos' src="./images/colunaSobrenos.png" alt="" />
-              </section>
+            </div>
+          
+        </div>
+        <div className='containerSobreNos2'>
+          <div className='containerColunaInfos'>
+          <img className='imgColunaSobreNos' src="./images/colunaSobrenos.png" alt="" />
+            <div>
+              <div>
+                  <span className='spanP'> Simplificar a experiência do usuário</span> 
+                      <p className='pSobrenos'>
+                      Criar uma interface organizada e eficiente, eliminando dificuldades na navegação.
+                        </p>
+              </div>
+              <div>
+                  <span className='spanP'>Facilitar a busca por freelancers</span> 
+                    <p className='pSobrenos'>
+                    Tornar a busca por freelancers rápida e prática, permitindo que o usuário encontre exatamente o que precisa com poucos cliques.
+                      </p>
+                </div>
             </div>
           </div>
-        </div>
-        <div>
-         
+          <div>
+              <img src="./images/imgSobreNosLading.png" alt="" className='imgSobreNosLading' />
+          </div>
+          </div>
+      </div>
+      <div className='containerEquipes'>
+        <h1 className='h1Equipe'>Equipes</h1>
+        <h2 className='h1Equipe'>Conheça as colaborações de cada um</h2>
+        <div className='containerIntegrantes'>
+          <div className='containerCaio'>
+            <div className='containerImgCaio'>
+              <img src="./icons/iconGit.png" alt="" className='iconSobreNos' />
+              <img src="./icons/iconLinkdin.png" alt="" className='iconSobreNos' />
+              <img src="./icons/iconInsta.png" alt=""  className='iconSobreNos'/>
+              
+            </div>
+            <h3 className='h3Equipe'>Responsavél por design e front-end.</h3>
+
+          </div>
+
+          <div className='containerVitor'>
+          <div className='containerImgVitor'>
+              <img src="./icons/iconGit.png" alt="" className='iconSobreNos' />
+              <img src="./icons/iconLinkdin.png" alt="" className='iconSobreNos' />
+              <img src="./icons/iconInsta.png" alt=""  className='iconSobreNos'/>
+              
+            </div>
+            <h3 className='h3Equipe'>Responsavél por design e front-end.</h3>
+          </div>
+          <div className='containerMaria'>
+          <div className='containerImgMaria'>
+              <img src="./icons/iconGit.png" alt="" className='iconSobreNos' />
+              <img src="./icons/iconLinkdin.png" alt="" className='iconSobreNos' />
+              <img src="./icons/iconInsta.png" alt=""  className='iconSobreNos'/>
+            </div>
+            <h3 className='h3Equipe'>Responsavél por design e front-end.</h3>
+          </div>
         </div>
       </div>
-      <div></div>
     </div>
    <Footer />
 </div>
