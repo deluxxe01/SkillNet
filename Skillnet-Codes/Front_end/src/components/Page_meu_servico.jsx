a9efa66fa4b437547d7ec990a765e070664bd474
caiolorramvalente <caio_valente@estudante.sesisenai.org.br>
2025-06-30 13:20:43 -0300

    s

--- Skillnet-Codes/Front_end/src/components/Page_meu_servico.jsx
+++ Skillnet-Codes/Front_end/src/components/Page_meu_servico.jsx
@@ -1,18 +1,17 @@
 import { useNavigate } from 'react-router-dom';
 import './Page_meu_servico.css';
-import { useContext } from 'react';
-import { GlobalContext } from '../context/Globalcontext'; 
+import { useContext, useState } from 'react';
+import { GlobalContext } from '../context/Globalcontext';
 
 function Page_meu_servico() {
   const { userLogado, cadastroServico } = useContext(GlobalContext);
   const navigate = useNavigate();
 
-  
+  const [modalAberto, setModalAberto] = useState(false);
+
   const servicosDoUsuario = Array.isArray(cadastroServico)
     ? cadastroServico.filter(s => Number(s.fk_usuario_id) === Number(userLogado.id_usuario))
     : [];
-
-  console.log("Serviços do usuário:", servicosDoUsuario);
 
   return (
     <div className='container_page_meu_serviço'>
@@ -23,8 +22,13 @@ function Page_meu_servico() {
 
         <div className="meuservico_lista">
           {servicosDoUsuario.map(servico => (
-            <div key={servico.servico_id}>
-              <h3 className="meuservico_titulo">{servico.titulo}</h3>
+            <div key={servico.servico_id} className='card_meu_servico'>
+              <img src={servico.imagem_capa || ''} className='poster_meu_servico' />
+              <p className='categoria_servico_especifico'>{servico.area}</p>
+              <p className='titulo_servico'>{servico.titulo}</p>
+              <p>{servico.descricao}</p>
+              <p>{servico.preco_minimo} R$</p>
+              <button onClick={() => setModalAberto(true)}>Editar serviço</button>
             </div>
           ))}
         </div>
@@ -33,6 +37,18 @@ function Page_meu_servico() {
           Cadastrar serviço
         </button>
       </div>
+
+    {modalAberto && (
+  <div className="modal-editar-overlay">
+    <div className="modal-editar-conteudo">
+      <h3>Editar Serviço</h3>
+      <p>Aqui você pode colocar os campos ou informações para editar o serviço.</p>
+      <div className="modal-editar-botoes">
+        <button onClick={() => setModalAberto(false)}>Fechar</button>
+      </div>
+    </div>
+  </div>
+)}
     </div>
   );
 }
